d7bc5ce40ed1f9835896323b4f8733a68d46ac71
Filip Gabrys <el.filberto@gmail.com>
2022-05-01 14:39:40 +0200

    First Init

---
+++ src/test/java/com/project/converter/ConverterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
